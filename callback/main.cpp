babac3d7673dbfc468acab82fb24c69d80a3a8c5
AceHammer17 <arh6766@g.rit.edu>
2021-10-07 16:11:32 -0400

    Basic callback

--- callback/main.cpp
+++ callback/main.cpp
@@ -3,12 +3,21 @@
 //
 #include <iostream>
 #include <node.h>
-#include <nan.h>
+using v8::Context;
+using v8::Function;
+using v8::FunctionCallbackInfo;
+using v8::FunctionTemplate;
+using v8::Isolate;
+using v8::Local;
+using v8::Number;
+using v8::Object;
+using v8::Persistent;
+using v8::String;
+using v8::Value;
+using v8::Array;
+using v8::Exception;
 using namespace v8;
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
 
 void Method(const FunctionCallbackInfo<Value>& args) {
     args.GetReturnValue().Set(args.Holder());
@@ -16,7 +25,8 @@ void Method(const FunctionCallbackInfo<Value>& args) {
 
 
 void init(Local<Object> exports) {
-    NODE_SET_METHOD(exports, "hello", Method);
+    NODE_SET_METHOD(exports, "RBG", Method);
 }
 
+//Name to call
 NODE_MODULE(callback_name, init)
